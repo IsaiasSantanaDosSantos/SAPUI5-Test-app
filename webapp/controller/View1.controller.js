b67eb22c890df1be2c46ee21f286f720c34f6b78
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2025-02-16 17:00:52 -0300

    feat: adjust list logic for show JSON information

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,25 +1,22 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"], function (Controller, JSONModel) {
   "use strict";
 
   return Controller.extend("zov.controller.View1", {
-    onInit() {
-      this.onShowValue();
-    },
-    onPress: function () {
-      alert("Hello Wold!");
+    onInit: function () {
+      this.loadProducts();
     },
 
-    // Função para carregar o arquivo JSON
-    onShowValue: function () {
-      const url = "../../data.json";
-      // Aqui você pode adicionar o código para buscar o JSON usando o modelo JSON
+    loadProducts: function () {
+      const url = "../../data.json"; 
+      const oModel = new JSONModel();
+
       fetch(url)
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+          oModel.setData(data); // Define os dados no modelo JSON
+          this.getView().setModel(oModel, "produtosModel"); // Define o modelo na View
+        })
         .catch((error) => console.log("Erro ao carregar o JSON:", error));
     },
   });
 });
-
-/*
- Agora preciso ajustar  aarquivo que renderiza (View1.view.xml), para renderizar as informações do JSON, conforme a função "onShowValue()"*/
